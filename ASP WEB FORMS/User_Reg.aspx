c4d7173d0bd1563da348bf2ad48f9508a04b5b52
cgvak-lt204 <cgvak-lt204@DESKTOP-TO03UD3>
2024-03-12 00:00:06 +0530

    Add project files.

---
+++ ASP WEB FORMS/User_Reg.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User_Reg.aspx.cs" Inherits="ASP_WEB_FORMS.User_Reg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <p>User Registration Details</p>
+
+        <asp:GridView ID="userGridView" runat="server" AutoGenerateColumns="true">
+        </asp:GridView>
+
+        <br />
+        <br />
+
+        <div>
+            Email<asp:TextBox ID="emailInput" runat="server"/> <br />
+            Password<asp:TextBox ID="passwordInput" runat="server" TextMode="Password" />  
+        </div>
+
+
+        <div>
+            <p>Create your Registration</p>
+            <asp:Button ID="addButton" Text="ADD" runat="server" OnClick="add_Click" />
+            <asp:Button ID="updateButton" Text="UPDATE" runat="server" OnClick="update_Click" />
+            <asp:Button ID="deleteButton" Text="DELETE" runat="server" OnClick="delete_Click" />
+            <asp:Button ID="readButton" Text="READ" runat="server" OnClick="read_Click" />
+        </div>
+    </form>
+
+
+</body>
+</html>
---
+++ ASP WEB FORMS/User_Reg.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ASP_WEB_FORMS
+{
+    public partial class User_Reg : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServerConnection"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        protected void add_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(emailInput.Text) && !string.IsNullOrWhiteSpace(passwordInput.Text))
+            {
+                string str = "INSERT INTO USER_REG VALUES(@eid, @pwd)";
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.Parameters.AddWithValue("@eid", emailInput.Text);
+                cmd.Parameters.AddWithValue("@pwd", passwordInput.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                LoadData();
+            }
+            else
+            {
+                Response.Write("<script>alert('Email and password cannot be empty');</script>");
+            }
+        }
+
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            string str = "DELETE FROM USER_REG WHERE EMAIL = @eid";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.Parameters.AddWithValue("@eid", emailInput.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            LoadData();
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            string str = "UPDATE USER_REG SET PASSWORD = @pwd WHERE EMAIL = @eid";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.Parameters.AddWithValue("@pwd", passwordInput.Text);
+            cmd.Parameters.AddWithValue("@eid", emailInput.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            LoadData();
+        }
+
+        protected void read_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT EMAIL, PASSWORD FROM USER_REG";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+
+            userGridView.DataSource = dt;
+            userGridView.DataBind();
+        }
+    }
+}
