67475a7ceb7de9d551cdaf7f1961d72d862ad26e
Haritanagarajan <30harita2002@gmail.com>
2024-03-20 10:55:36 +0530

    2nd commit

---
+++ ASP WEB FORMS/Snacks.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Snacks.aspx.cs" Inherits="ASP_WEB_FORMS.Snacks" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Snacks Inventory</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Snacks Inventory</h2>
+            <hr />
+            <table>
+                <tr>
+                    <td>Snack Name:</td>
+                    <td><asp:TextBox ID="txtSnackName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Snack Amount:</td>
+                    <td><asp:TextBox ID="txtSnackAmount" runat="server" TextMode="Number"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Snack ID:</td>
+                    <td><asp:TextBox ID="txtId" runat="server"></asp:TextBox></td>
+                </tr>
+            </table>
+            <br />
+            <asp:Button ID="btnInsert" runat="server" Text="Insert Snack" OnClick="btnInsert_Click" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update Snack" OnClick="btnUpdate_Click" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete Snack" OnClick="btnDelete_Click" />
+            <hr />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                    <asp:BoundField DataField="snackName" HeaderText="Snack Name" />
+                    <asp:BoundField DataField="snackAmount" HeaderText="Snack Amount" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP WEB FORMS/Snacks.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace ASP_WEB_FORMS
+{
+    public partial class Snacks : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        private SqlConnection ConnectDB()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SqlServerConnection"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        private void ExecuteStoredProcedure(string procedureName, SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = ConnectDB())
+            {
+                SqlCommand command = new SqlCommand(procedureName, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void BindGrid()
+        {
+            using (SqlConnection connection = ConnectDB())
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM snacks", connection);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlParameter[] parameters = {
+                new SqlParameter("@snackName", txtSnackName.Text),
+                new SqlParameter("@snackAmount", Convert.ToInt32(txtSnackAmount.Text))
+            };
+            ExecuteStoredProcedure("InsertSnack", parameters);
+            BindGrid();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlParameter[] parameters = {
+                new SqlParameter("@Id", Convert.ToInt32(txtId.Text)),
+                new SqlParameter("@snackName", txtSnackName.Text),
+                new SqlParameter("@snackAmount", Convert.ToInt32(txtSnackAmount.Text))
+            };
+            ExecuteStoredProcedure("UpdateSnack", parameters);
+            BindGrid();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlParameter[] parameters = {
+                new SqlParameter("@Id", Convert.ToInt32(txtId.Text))
+            };
+            ExecuteStoredProcedure("DeleteSnack", parameters);
+            BindGrid();
+        }
+    }
+}
