3d8dcf6e152d3986c28476438b0ab19288ebe7cf
Haritanagarajan <30harita2002@gmail.com>
2024-04-07 13:42:34 +0530

    Task 2 Login Page

---
+++ ASP WEB FORMS/LoginForm.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginForm.aspx.cs" Inherits="ASP_WEB_FORMS.LoginForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Form</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Login Form</h2>
+            <div>
+                <label for="txtID">ID:</label>
+                <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtID" ErrorMessage="ID is required" ForeColor="Red"></asp:RequiredFieldValidator>
+            </div>
+            <div>
+                <label for="txtName">Name:</label>
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtName" ErrorMessage="Name is required" ForeColor="Red"></asp:RequiredFieldValidator>
+            </div>
+            <div>
+                <label for="txtPassword">Password:</label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtPassword" ErrorMessage="Password is required" ForeColor="Red"></asp:RequiredFieldValidator>
+            </div>
+            <div>
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP WEB FORMS/LoginForm.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace ASP_WEB_FORMS
+{
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Validate the input fields
+            if (Page.IsValid)
+            {
+                // Authenticate user
+                if (AuthenticateUser(txtID.Text, txtName.Text, txtPassword.Text))
+                {
+                    // Redirect to CRUD page if authentication succeeds
+                    Response.Redirect("Snacks.aspx");
+                }
+                else
+                {
+                    // Display error message if authentication fails
+                    Response.Write("<script>alert('Invalid credentials. Please try again.');</script>");
+                }
+            }
+        }
+
+        // Method to authenticate user against database
+        private bool AuthenticateUser(string id, string name, string password)
+        {
+            // Connection string to your database
+            string connectionString = WebConfigurationManager.ConnectionStrings["SqlServerConnection"].ConnectionString;
+
+            // SQL query to check if provided credentials exist in the database
+            string query = "SELECT COUNT(*) FROM EMPLOYEE_TABLE WHERE ID = @ID AND NAME = @Name AND PASSWORD = @Password";
+
+            // Using statement ensures that resources are freed after execution
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to the query to prevent SQL injection
+                    command.Parameters.AddWithValue("@ID", id);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    // Open the connection
+                    connection.Open();
+
+                    // Execute the query and get the result
+                    int count = (int)command.ExecuteScalar();
+
+                    // If count is greater than 0, user exists in the database
+                    return count > 0;
+                }
+            }
+        }
+    }
+}
